1b167cac1cb7320e6f9935c1d0e61f0985dc1a78
Michel_Cavalcante_Dias <michel.dias-rpa@bemobi.com>
2023-10-11 22:14:16 -0300

    create web server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	handler "mercado-livre-integration/internal/handler"
+	"mercado-livre-integration/internal/infrastructure/client"
+	"mercado-livre-integration/internal/service"
+	"net/http"
+	"time"
+)
+
+//TODO: colocar aqui um init
 
 func main() {
-	fmt.Println("Hello, World!")
+	client := client.NewMercadoLivre("", "", "", "", time.Hour*2)
+	service := service.NewCategory(client)
+	categoryHandler := handler.NewCategory(service)
+	r := mux.NewRouter()
+
+	r.HandleFunc("sites/{siteID}/categories", categoryHandler.GetCategories).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
