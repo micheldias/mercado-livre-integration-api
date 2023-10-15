4023fbc7f352d07756cb5f837499a7541cb3e93b
Michel_Cavalcante_Dias <michel.dias-rpa@bemobi.com>
2023-10-15 16:04:03 -0300

    create server and use viper

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+	"io"
 	"log"
 	handler "mercado-livre-integration/internal/handler"
 	"mercado-livre-integration/internal/infrastructure/client"
@@ -10,15 +13,51 @@ import (
 	"time"
 )
 
-//TODO: colocar aqui um init
+func init() {
+	viper.SetConfigName("application")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+}
 
 func main() {
-	client := client.NewMercadoLivre("", "", "", "", time.Hour*2)
-	service := service.NewCategory(client)
-	categoryHandler := handler.NewCategory(service)
+	client := client.NewMercadoLivre(
+		viper.GetString("MERCADO_LIVRE_CLIENT_ID"),
+		viper.GetString("MERCADO_LIVRE_SECRET"),
+		viper.GetString("MERCADO_LIVRE_REDIRECT_URL"),
+		viper.GetString("MERCADO_LIVRE_API_URL"),
+		viper.GetDuration("MERCADO_LIVRE_EXECUTE_TIMES"),
+	)
+	categoryService := service.NewCategory(client)
+	categoryHandler := handler.NewCategory(categoryService)
+
+	tokenService := service.NewToken(client)
+	tokenHandler := handler.NewToken(tokenService)
 	r := mux.NewRouter()
+	r.HandleFunc("/health", HealthCheckHandler).Methods(http.MethodGet)
+	s := r.PathPrefix("/api/v1/").Subrouter()
+	s.HandleFunc("sites/{siteID}/categories", categoryHandler.GetCategories).Methods(http.MethodGet)
+	s.HandleFunc("/tokens", tokenHandler.Create).Methods(http.MethodPost)
 
-	r.HandleFunc("sites/{siteID}/categories", categoryHandler.GetCategories).Methods(http.MethodGet)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(srv.ListenAndServe())
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// A very simple health check.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// In the future we could report back on the status of our DB, or our cache
+	// (e.g. Redis) by performing a simple PING, and include them in the response.
+	io.WriteString(w, `{"alive": true}`)
 }
