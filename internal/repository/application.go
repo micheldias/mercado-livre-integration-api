35f46563ad14ca2cecafa6ae55745af8d303051e
Michel_Cavalcante_Dias <michel.dias-rpa@bemobi.com>
2024-01-15 20:20:15 -0300

    instrument nr transaction

--- internal/repository/application.go
+++ internal/repository/application.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"github.com/newrelic/go-agent/v3/newrelic"
 	"gorm.io/gorm"
 	"mercado-livre-integration/internal/model"
 )
@@ -24,24 +23,18 @@ type appRepository struct {
 }
 
 func (a appRepository) GetByID(ctx context.Context, id int) (model.Application, error) {
-	txn := newrelic.FromContext(ctx)
-	gormTransactionContext := newrelic.NewContext(context.Background(), txn)
 	var app model.Application
-	if err := a.dbConnection.WithContext(gormTransactionContext).First(&app, id).Error; err != nil {
+	if err := a.dbConnection.WithContext(ctx).First(&app, id).Error; err != nil {
 		return app, err
 	}
-	//segment.End()
 	return app, nil
 }
 
 func (a appRepository) Get(ctx context.Context) ([]model.Application, error) {
-	txn := newrelic.FromContext(ctx)
-	txn.StartSegment("TestDB")
 	var apps []model.Application
 	if err := a.dbConnection.WithContext(ctx).Find(&apps).Error; err != nil {
 		return apps, err
 	}
-	txn.End()
 	return apps, nil
 }
 
